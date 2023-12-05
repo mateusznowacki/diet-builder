f0ad0fe7d22cd1f619b6827cff45d2e40a76b8eb
Mateusz <mattynovy@outlook.com>
2023-12-05 16:12:16 +0100

    working add product

--- src/main/java/pl/dietbuilder/controllers/product/EditProductController.java
+++ src/main/java/pl/dietbuilder/controllers/product/EditProductController.java
@@ -1,4 +1,77 @@
 package pl.dietbuilder.controllers.product;
 
-public class EditProductController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import pl.dietbuilder.dbmanagement.ConnectionManager;
+import pl.dietbuilder.dbmanagement.ProductDAO;
+import pl.dietbuilder.model.Product;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditProductController implements Initializable {
+
+    @FXML
+    private TableColumn<Product, String> categoryColumn;
+
+    @FXML
+    private TableColumn<Product, Integer> idColumn;
+
+    @FXML
+    private TableColumn<Product, String> nameColumn;
+
+    @FXML
+    private TableView<Product> productTableView;
+
+    @FXML
+    private TextField searchBar;
+
+    ObservableList<Product> productsObservableList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        ProductDAO productDAO = new ProductDAO(ConnectionManager.getInstance().getConnection());
+        productsObservableList.addAll(productDAO.getAllProducts());
+
+
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+
+        productTableView.setItems(productsObservableList);
+        FilteredList<Product> filteredData = new FilteredList<>(productsObservableList, p -> true);
+        searchBar.textProperty().addListener((observable, oldValue, newValue) -> filteredData.setPredicate(product -> {
+            if (newValue.isEmpty() || newValue == null || newValue.isBlank()) {
+                return true;
+            }
+
+            String keywordSearch = newValue.toLowerCase();
+            if (product.getName().toLowerCase().indexOf(keywordSearch) > -1) {
+                return true;
+            } else if (product.getCategory().toLowerCase().indexOf(keywordSearch) > -1) {
+                return true;
+            } else if (String.valueOf(product.getId()).indexOf(keywordSearch) > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        }));
+        SortedList<Product> sortedData = new SortedList<>(filteredData);
+
+        sortedData.comparatorProperty().bind(productTableView.comparatorProperty());
+
+        productTableView.setItems(sortedData);
+
+    }
 }
+
+
--- src/main/java/pl/dietbuilder/dbmanagement/ProductDAO.java
+++ src/main/java/pl/dietbuilder/dbmanagement/ProductDAO.java
@@ -4,7 +4,9 @@ import pl.dietbuilder.model.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ProductDAO {
     private final Connection connection;
@@ -28,6 +30,33 @@ public class ProductDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public ArrayList<Product> getAllProducts() {
+        ArrayList<Product> productsList = new ArrayList<>();
+
+        String sql = "SELECT * FROM product";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String category = resultSet.getString("category");
+                double calories = resultSet.getDouble("energy");
+                double carbohydrates = resultSet.getDouble("carbohydrates");
+                double fats = resultSet.getDouble("fat");
+                double proteins = resultSet.getDouble("protein");
+
+                Product product = new Product(id, name, category, calories, proteins, fats, carbohydrates);
+                productsList.add(product);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productsList;
+
     }
 
 }
