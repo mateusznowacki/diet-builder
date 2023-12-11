89011d6fd126f282a79c8c8f3d943238c757151b
Mateusz <mattynovy@outlook.com>
2023-12-11 09:09:18 +0100

    working app

---
+++ src/main/java/pl/dietbuilder/app/App.java
@@ -0,0 +1,31 @@
+package pl.dietbuilder.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/main.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Diet Builder");
+        primaryStage.show();
+        primaryStage.setResizable(false);
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
--- src/main/java/pl/dietbuilder/app/Main.java
+++ src/main/java/pl/dietbuilder/app/Main.java
@@ -1,32 +1,7 @@
 package pl.dietbuilder.app;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import pl.dietbuilder.dbmanagement.CategoryDAO;
-import pl.dietbuilder.dbmanagement.ConnectionManager;
-
-import java.util.Objects;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/main.fxml")));
-        Scene scene = new Scene(root);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Diet Builder");
-        primaryStage.show();
-        primaryStage.setResizable(false);
-
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        App.main(args);
     }
 }
